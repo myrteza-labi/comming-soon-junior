2f70e2ef97a116f083be05d9b2489a7a103ec69f
Myrteza <myrteza@hotmail.fr>
2021-10-22 14:19:43 +0200

    :style: (static)  mobile and desktop style done

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,10 @@ import './Input.css';
 
 function Input(props){
     return (
-        <div className="Input" ></div>
+        <div className="Input" >
+            <input className="inputEmail inputEmailError" type="email" placeholder="Your email adress..." />
+            <p className="errorText" >Please proide a valid email address</p>
+        </div>
     )
 }
 
--- src/components/TextPresentation.js
+++ src/components/TextPresentation.js
@@ -4,7 +4,9 @@ import './TextPresentation.css';
 function TextPresentation(props){
     return (
         <div className="TextPresentation" >
-            <h2 className="textPresentationTitle" >We are launching soon!</h2>
+            <h2 className="textPresentationTitle" >We are launching  
+                <span className="titleImportantWord" > soon! </span>
+            </h2>
             <p className="textPresentationParagraph" >Subscribe and get notified</p>
         </div>
     )
